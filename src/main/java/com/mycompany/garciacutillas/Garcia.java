96d1b68ca18d80c0692d0b96f60e3f403dd05c75
Francisco José García Cutillas <3979199@alu.murciaeduca.es>
2023-05-20 17:06:19 +0200

    Se añade a la clase Garcia método divideEnteros y divideDouble, y a la clase Cutillas método devuelveFechaLocalDate y devuelveDiaMes

--- src/main/java/com/mycompany/garciacutillas/Garcia.java
+++ src/main/java/com/mycompany/garciacutillas/Garcia.java
@@ -12,4 +12,16 @@ public class Garcia {
         
     }
     
+    public static int divideEnteros (int num1, int num2){
+        
+        return num1 / num2;
+        
+    }
+    
+    public static double divideDouble (double num1, double num2){
+        
+        return num1 / num2;
+        
+    }
+    
 }
